3b1a524d5e40d760519a0271ecc0dc96a87b0c36
Aurélien Mino <aurelien.mino@gmail.com>
2011-12-22 21:33:52 +0100

    Added support for using links from fr.wikipedia.org to determine country.

--- wp_artist_country.py
+++ wp_artist_country.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
 import re
 import sqlalchemy
@@ -9,11 +12,13 @@ import time
 from utils import mangle_name, join_names, mw_remove_markup
 import config as cfg
 
+wp_lang = 'fr'
+
 engine = sqlalchemy.create_engine(cfg.MB_DB)
 db = engine.connect()
 db.execute("SET search_path TO musicbrainz")
 
-wp = MediaWiki('http://en.wikipedia.org/w/api.php')
+wp = MediaWiki('http://'+wp_lang+'.wikipedia.org/w/api.php')
 
 mb = MusicBrainzClient(cfg.MB_USERNAME, cfg.MB_PASSWORD, cfg.MB_SITE)
 
@@ -37,22 +42,22 @@ LEFT JOIN bot_wp_artist_country b ON a.gid = b.gid
 WHERE
     b.gid IS NULL AND
     a.country IS NULL AND
-    u.url LIKE 'http://en.wikipedia.org/wiki/%%'
+    u.url LIKE 'http://"""+wp_lang+""".wikipedia.org/wiki/%%'
 ORDER BY a.id
 LIMIT 10000
 """
 
 
 def get_page_content_from_cache(title):
-    key = title.encode('ascii', 'xmlcharrefreplace').replace('/', '_')
-    file = os.path.join('enwiki-cache', key[0], key)
+    key = title.encode('utf-8', 'xmlcharrefreplace').replace('/', '_')
+    file = os.path.join('wiki-cache', wp_lang, key[0], key)
     if os.path.exists(file):
         return open(file).read().decode('utf8')
 
 
 def add_page_content_to_cache(title, content):
-    key = title.encode('ascii', 'xmlcharrefreplace').replace('/', '_')
-    dir = os.path.join('enwiki-cache', key[0])
+    key = title.encode('utf-8', 'xmlcharrefreplace').replace('/', '_')
+    dir = os.path.join('wiki-cache', wp_lang, key[0])
     if not os.path.exists(dir):
         os.mkdir(dir)
     file = os.path.join(dir, key)
@@ -65,7 +70,7 @@ def get_page_content(wp, title):
     content = get_page_content_from_cache(title)
     if content:
         return content
-    resp = wp.call({'action': 'query', 'prop': 'revisions', 'titles': title, 'rvprop': 'content'})
+    resp = wp.call({'action': 'query', 'prop': 'revisions', 'titles': title.encode('utf8'), 'rvprop': 'content'})
     pages = resp['query']['pages'].values()
     if not pages or 'revisions' not in pages[0]:
         return None
@@ -75,16 +80,21 @@ def get_page_content(wp, title):
 
 
 def extract_page_title(url):
-    prefix = 'http://en.wikipedia.org/wiki/'
+    prefix = 'http://' + wp_lang + '.wikipedia.org/wiki/'
     if not url.startswith(prefix):
         return None
     return urllib.unquote(url[len(prefix):].encode('utf8')).decode('utf8')
 
+category_re = {}
+category_re['en'] = re.compile(r'\[\[Category:(.+?)\]\]')
+category_re['fr'] = re.compile(r'\[\[Cat\xe9gorie:(.+?)\]\]')
+infobox_re = {}
+infobox_re['en'] = re.compile(r'\{\{Infobox (musical artist|person)[^|]*((?:[^{}].*?|\{\{.*?\}\})*)\}\}', re.DOTALL)
+infobox_re['fr'] = re.compile(r'\{\{Infobox (Musique \(artiste\)|Musique classique \(personnalit\xe9\))[^|]*((?:[^{}].*?|\{\{.*?\}\})*)\}\}', re.DOTALL)
 
-category_re = re.compile(r'\[\[Category:(.+?)\]\]')
-infobox_re = re.compile(r'\{\{Infobox (musical artist|person)[^|]*((?:[^{}].*?|\{\{.*?\}\})*)\}\}', re.DOTALL)
+link_countries = {}
 
-link_countries = {
+link_countries['en'] = {
     'Afghanistan': 'AF',
     'Albania': 'AL',
     'Algeria': 'DZ',
@@ -345,6 +355,275 @@ link_countries = {
     'Korean': 'ZW',
 }
 
+link_countries['fr'] = {
+    'Afghanistan': 'AF',
+    'Albanie': 'AL',
+    'Algérie': 'DZ',
+    'American Samoa': 'AS',
+    'Andorre': 'AD',
+    'Angola': 'AO',
+    'Anguilla': 'AI',
+    'Antarctique': 'AQ',
+    'Antigua and Barbuda': 'AG',
+    'Argentine': 'AR',
+    'Arménie': 'AM',
+    'Aruba': 'AW',
+    'Australie': 'AU',
+    'Autriche': 'AT',
+    'Azerbaijan': 'AZ',
+    'Bahamas': 'BS',
+    'Bahrain': 'BH',
+    'Bangladesh': 'BD',
+    'Barbados': 'BB',
+    'Belarus': 'BY',
+    'Belgique': 'BE',
+    'Belize': 'BZ',
+    'Benin': 'BJ',
+    'Bermuda': 'BM',
+    'Bhutan': 'BT',
+    'Bolivia': 'BO',
+    'Bosnia and Herzegovina': 'BA',
+    'Botswana': 'BW',
+    'Bouvet Island': 'BV',
+    'Brézil': 'BR',
+    'Brazilia': 'BR',
+    'Brisbane': 'AU',
+    'British Indian Ocean Territory': 'IO',
+    'Brunei Darussalam': 'BN',
+    'Bulgarie': 'BG',
+    'Burkina Faso': 'BF',
+    'Burundi': 'BI',
+    'Cambodge': 'KH',
+    'Cameroun': 'CM',
+    'Canada': 'CA',
+    'Canadiens': 'CA',
+    'Cape Verde': 'CV',
+    'Cayman Islands': 'KY',
+    'République centrafricaine': 'CF',
+    'Chad': 'TD',
+    'Chili': 'CL',
+    'Chine': 'CN',
+    'Christmas Island': 'CX',
+    'Cocos (Keeling) Islands': 'CC',
+    'Colombie': 'CO',
+    'Comoros': 'KM',
+    'Congo': 'CG',
+    'République du Congo': 'CG',
+    'République démocratique du Congo': 'CD',
+    'Cook Islands': 'CK',
+    'Costa Rica': 'CR',
+    'Croatie': 'HR',
+    'Cuba': 'CU',
+    'Cyprus': 'CY',
+    'Czech Republic': 'CZ',
+    'Danemark': 'DK',
+    'Djibouti': 'DJ',
+    'Dominica': 'DM',
+    'East Germany': 'DE',
+    'Équateur (pays)': 'EC',
+    'Égypte': 'EG',
+    'El Salvador': 'SV',
+    'Angleterre': 'GB',
+    'Equatorial Guinea': 'GQ',
+    'Eritrea': 'ER',
+    'Estonie': 'EE',
+    'Éthiopie': 'ET',
+    'Falkland Islands (Malvinas)': 'FK',
+    'Faroe Islands': 'FO',
+    'Fiji': 'FJ',
+    'Finlande': 'FI',
+    'France': 'FR',
+    'Guyane': 'GF',
+    'Polynésie française': 'PF',
+    'Terres australes et antarctiques françaises': 'TF',
+    'Gabon': 'GA',
+    'Gambia': 'GM',
+    'Géorgie': 'GE',
+    'Allemagne': 'DE',
+    'Ghana': 'GH',
+    'Gibraltar': 'GI',
+    'Grèce': 'GR',
+    'Greenland': 'GL',
+    'Grenada': 'GD',
+    'Guadeloupe': 'GP',
+    'Guam': 'GU',
+    'Guatemala': 'GT',
+    'Guernsey': 'GG',
+    'Guinée-Bissau': 'GW',
+    'Guinée': 'GN',
+    'Guyana': 'GY',
+    'Haiti': 'HT',
+    'Heard Island and McDonald Islands': 'HM',
+    'Honduras': 'HN',
+    'Hong Kong': 'HK',
+    'Hungary': 'HU',
+    'Iceland': 'IS',
+    'Inde': 'IN',
+    'Indonésie': 'ID',
+    'Iran, Islamic Republic of': 'IR',
+    'Iraq': 'IQ',
+    'Irelande': 'IE',
+    'Isle of Man': 'IM',
+    'Israel': 'IL',
+    'Italian people': 'IT',
+    'Italie': 'IT',
+    'Jamaïque': 'JM',
+    'Japon': 'JP',
+    'Tokyo': 'JP',
+    'Jersey': 'JE',
+    'Jordan': 'JO',
+    'Kazakhstan': 'KZ',
+    'Kenya': 'KE',
+    'Royaume des Pays-Bas': 'NL',
+    'Kiribati': 'KI',
+    'Corée du Sud': 'KR',
+    'Koweït': 'KW',
+    'Kirghizistan': 'KG',
+    'Latvia': 'LV',
+    'Liban': 'LB',
+    'Lesotho': 'LS',
+    'Libéria': 'LR',
+    'Liberia': 'LR',
+    'Libyan Arab Jamahiriya': 'LY',
+    'Liechtenstein': 'LI',
+    'Lituanie': 'LT',
+    'Londres': 'GB',
+    'Luxembourg': 'LU',
+    'Macao': 'MO',
+    'Macédoine (pays)': 'MK',
+    'Madagascar': 'MG',
+    'Malawi': 'MW',
+    'Malaisie': 'MY',
+    'Maldives': 'MV',
+    'Mali': 'ML',
+    'Malte': 'MT',
+    'Manchester': 'GB',
+    'Îles Marshall': 'MH',
+    'Martinique': 'MQ',
+    'Mauritanie': 'MR',
+    'Maurice (pays)': 'MU',
+    'Mayotte': 'YT',
+    'Mexique': 'MX',
+    'Micronesia, Federated States of': 'FM',
+    'Moldavie': 'MD',
+    'Monaco': 'MC',
+    'Mongolie': 'MN',
+    'Monténégro': 'ME',
+    'Montserrat': 'MS',
+    'Maroc': 'MA',
+    'Mozambique': 'MZ',
+    'Myanmar': 'MM',
+    'Namibie': 'NA',
+    'Nauru': 'NR',
+    'Népal': 'NP',
+    'Antilles néerlandaises': 'AN',
+    'Pays-Bas': 'NL',
+    'Nouvelle-Calédonie': 'NC',
+    'Nouvelle-Zélande': 'NZ',
+    'Nicaragua': 'NI',
+    'Nigeria': 'NG',
+    'Nigéria': 'NG',
+    'Niger': 'NE',
+    'Niue': 'NU',
+    'Norfolk Island': 'NF',
+    'Northern Mariana Islands': 'MP',
+    'Norvège': 'NO',
+    'Oman': 'OM',
+    'Pakistan': 'PK',
+    'Palau': 'PW',
+    'Paris': 'FR',
+    'État de Palestine': 'PS',
+    'Panama': 'PA',
+    'Papouasie-Nouvelle-Guinée': 'PG',
+    'Paraguay': 'PY',
+    'Pérou': 'PE',
+    'Philippines': 'PH',
+    'Pitcairn': 'PN',
+    'Pologne': 'PL',
+    'Portugal': 'PT',
+    'Puerto Rico': 'PR',
+    'Qatar': 'QA',
+    'Katar': 'QA',
+    'Réunion': 'RE',
+    'Roumanie': 'RO',
+    'Russie': 'RU',
+    'Fédération de Russie': 'RU',
+    'Rwanda': 'RW',
+    'Saint-Barthélemy': 'BL',
+    'Saint Helena': 'SH',
+    'Saint Kitts and Nevis': 'KN',
+    'Saint Lucia': 'LC',
+    'Saint Martin': 'MF',
+    'Saint-Pierre-et-Miquelon': 'PM',
+    'Saint Vincent and The Grenadines': 'VC',
+    'Samoa': 'WS',
+    'San Marino': 'SM',
+    'Sao Tome and Principe': 'ST',
+    'Saudi Arabia': 'SA',
+    'Écosse': 'GB',
+    'Sénégal': 'SN',
+    'Serbie-et-Monténégro': 'CS',
+    'Serbie': 'RS',
+    'Seychelles': 'SC',
+    'Sierra Leone': 'SL',
+    'Singapour': 'SG',
+    'Slovaquie': 'SK',
+    'Slovénie': 'SI',
+    'Solomon Islands': 'SB',
+    'Somalie': 'SO',
+    'Afrique du Sud': 'ZA',
+    'South Georgia and the South Sandwich Islands': 'GS',
+    'Espagne': 'ES',
+    'Sri Lanka': 'LK',
+    'Soudan': 'SD',
+    'Suriname': 'SR',
+    'Svalbard and Jan Mayen': 'SJ',
+    'Swaziland': 'SZ',
+    'Suède': 'SE',
+    'Suisse': 'CH',
+    'Confédération suisse': 'CH',
+    'Syrian Arab Republic': 'SY',
+    'Taïwan': 'TW',
+    'Tadjikistan': 'TJ',
+    'Tanzania, United Republic of': 'TZ',
+    'Thaïlande': 'TH',
+    'Timor-Leste': 'TL',
+    'Togo': 'TG',
+    'Tokelau': 'TK',
+    'Tonga': 'TO',
+    'Trinité-et-Tobago': 'TT',
+    'Tunisie': 'TN',
+    'Turquie': 'TR',
+    'Turkménistan': 'TM',
+    'Turks and Caicos Islands': 'TC',
+    'Tuvalu': 'TV',
+    'Ouganda': 'UG',
+    'Ukraine': 'UA',
+    'Émirats arabes unis': 'AE',
+    'Royaume Uni': 'GB',
+    'Royaume-Uni': 'GB',
+    'United States Minor Outlying Islands': 'UM',
+    'États-Unis': 'US',
+    'Uruguay': 'UY',
+    'USA': 'US',
+    'Uzbekistan': 'UZ',
+    'Vanuatu': 'VU',
+    'Vatican City State (Holy See)': 'VA',
+    'Venezuela': 'VE',
+    'Vénézuéla': 'VE',
+    'Viêt Nam': 'VN',
+    'Virgin Islands, British': 'VG',
+    'Virgin Islands, U.S.': 'VI',
+    'Pays de Galles': 'GB',
+    'Wallis-et-Futuna': 'WF',
+    'Western Sahara': 'EH',
+    'Yémen': 'YE',
+    'Yougoslavie': 'YU',
+    'Zambia': 'ZM',
+    'Zimbabwe': 'ZW',
+    'Québec': 'CA'
+}
+
 link_us_states = [
     'Alabama',
     'Alaska',
@@ -400,7 +679,9 @@ link_us_states = [
     'Wyoming',
 ]
 
-category_countries = {
+category_countries = {}
+
+category_countries['en'] = {
     'American': 'US',
     'English': 'GB',
     'British': 'GB',
@@ -454,9 +735,70 @@ category_countries = {
     'Hong Kong': 'HK',
 }
 
+category_countries['fr'] = {
+    'américain': 'US',
+    'anglais': 'GB',
+    'britannique': 'GB',
+    'écossais': 'GB',
+    'gallois': 'GB',
+    'belge': 'BE',
+    'hollandais': 'NL',
+    'français': 'FR',
+    'polonais': 'PL',
+    'suisse': 'CH',
+    'italien': 'IT',
+    'pakistanais': 'PK',
+    'espagnol': 'ES',
+    'allemand': 'DE',
+    'grec': 'GR',
+    'australien': 'AU',
+    'canadien': 'CA',
+    'québécois': 'CA',
+    'indonésien': 'ID',
+    'suédois': 'SE',
+    'brésilien': 'BR',
+    'irlandais': 'IE',
+    'coréen': 'KR',
+    'japonais': 'JP',
+    'chinois': 'CN',
+    'russe': 'RU',
+    'hongrois': 'HU',
+    'slovaque': 'SK',
+    'tchèque': 'CZ',
+    'ukrainien': 'UA',
+    'turc': 'TR',
+    'roumain': 'RO',
+    'polonais': 'PL',
+    'portugais': 'PT',
+    'norvégien': 'NO',
+    'marocain': 'MA',
+    'mexicain': 'MX',
+    'letton': 'LV',
+    'estonien': 'EE',
+    'finlandais': 'FI',
+    'autrichien': 'AT',
+    'israélien': 'IL',
+    'sud-africain': 'ZA',
+    'Puerto Rican': 'PR',
+    'sénégalais': 'SN',
+    'croate': 'HR',
+    'danois': 'DK',
+    'islandais': 'IS',
+    'jamaïcain': 'JM',
+    'nouveau-zélandais': 'NZ',
+    'algérien': 'DZ',
+    'cubain': 'CU',
+    'Hong Kong': 'HK',
+    'tunisien': 'TN',
+    'monégasque': 'MC'
+}
+
+infobox_fields_country = {}
+infobox_fields_country['en'] = ['origin', 'born', 'birth_place']
+infobox_fields_country['fr'] = ['naissance lieu', 'décès lieu', 'nationalité', 'pays origine']
 
 def parse_infobox(page):
-    match = infobox_re.search(page)
+    match = infobox_re[wp_lang].search(page)
     info = {}
     if match is None:
         return info
@@ -477,11 +819,11 @@ def extract_first_paragraph(page):
 def determine_country_from_categories(page):
     countries = set()
     relevant_categories = []
-    categories = category_re.findall(page)
+    categories = category_re[wp_lang].findall(page)
     for category in categories:
         category = category.replace('_', ' ')
-        for name, code in category_countries.iteritems():
-            if category.startswith(name + ' '):
+        for name, code in category_countries[wp_lang].iteritems():
+            if name.decode('utf8') in category:
                 countries.add(code)
                 relevant_categories.append(category)
     reason = 'Belongs to %s.' % join_names('category', relevant_categories)
@@ -490,9 +832,9 @@ def determine_country_from_categories(page):
 
 def find_countries_in_text(countries, relevant_links, text):
     text = text.replace('_', ' ')
-    for name, code in link_countries.iteritems():
-        for name in [name, name.lower()]:
-            if '[[' + name + ']]' in text or '[[' + name + '|' in text:
+    for name, code in link_countries[wp_lang].iteritems():
+        for name in [name.decode('utf8'), name.decode('utf8').lower()]:
+            if '[[' + name + ']]' in text or '[[' + name + '|' in text or '{{' + name + '}}' in text:
                 countries.add(code)
                 relevant_links.append(name)
                 break
@@ -506,8 +848,11 @@ def find_countries_in_text(countries, relevant_links, text):
 def determine_country_from_infobox(infobox):
     countries = set()
     relevant_links = []
-    for field in ['origin', 'born', 'birth_place']:
+    for field in infobox_fields_country[wp_lang]:
+        field = field.decode('utf8')
         text = infobox.get(field, '')
+        if len(text) > 0:
+            print "Text from infobox (field=%s): %s" % (field, text)
         find_countries_in_text(countries, relevant_links, text)
     reason = 'Infobox links to %s.' % join_names('', relevant_links)
     return countries, reason
