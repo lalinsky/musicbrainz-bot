9bfbf9983ce76eccbb00e7fc67072773af7073ba
Aurélien Mino <aurelien.mino@gmail.com>
2012-01-02 12:25:54 +0100

    Added support for linking to fr.wikipedia.org for artists without wikipedia links.

--- wp_links_artists.py
+++ wp_links_artists.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import re
 import sqlalchemy
 import solr
@@ -13,20 +15,25 @@ engine = sqlalchemy.create_engine(cfg.MB_DB)
 db = engine.connect()
 db.execute("SET search_path TO musicbrainz")
 
-wp = MediaWiki('http://en.wikipedia.org/w/api.php')
-wps = solr.SolrConnection('http://localhost:8983/solr/wikipedia')
+wp_lang = 'fr'
+
+wp = MediaWiki('http://'+wp_lang+'.wikipedia.org/w/api.php')
+
+suffix = '_' + wp_lang if wp_lang != 'en' else ''
+wps = solr.SolrConnection('http://localhost:8983/solr/wikipedia'+suffix)
 
 mb = MusicBrainzClient(cfg.MB_USERNAME, cfg.MB_PASSWORD, cfg.MB_SITE)
 
 """
 
-CREATE TABLE bot_wp_artist (
+CREATE TABLE bot_wp_artist_link (
     gid uuid NOT NULL,
+    lang character varying(2),
     processed timestamp with time zone DEFAULT now()
 );
 
-ALTER TABLE ONLY bot_wp_artist
-    ADD CONSTRAINT bot_wp_artist_pkey PRIMARY KEY (gid);
+ALTER TABLE ONLY bot_wp_artist_link
+    ADD CONSTRAINT bot_wp_artist_link_pkey PRIMARY KEY (gid, lang);
 
 CREATE VIEW v_artist_rgs AS
     SELECT acn.artist, count(*) AS count FROM (s_release_group rg JOIN artist_credit_name acn ON ((rg.artist_credit = acn.artist_credit))) WHERE (acn.artist > 2) GROUP BY acn.artist;
@@ -45,20 +52,28 @@ ORDER BY ar.count DESC
 LIMIT 1000
 """
 
+acceptable_countries_for_lang = {
+    'en': [],
+    'fr': ['FR', 'MC']
+}
+
 query = """
 WITH
     artists_wo_wikipedia AS (
-        SELECT a.id
+        SELECT DISTINCT a.id
         FROM artist a
+        LEFT JOIN country c ON c.id = a.country
+            AND c.iso_code IN ("""+ ', '.join('%s' for unused in acceptable_countries_for_lang[wp_lang]) +""")
         LEFT JOIN l_artist_url l ON
             l.entity0 = a.id AND
             l.link IN (SELECT id FROM link WHERE link_type = 179)
         WHERE a.id > 2 AND l.id IS NULL
+            AND (c.iso_code IS NOT NULL OR %s = 'en')
     )
 SELECT a.id, a.gid, a.name
 FROM artists_wo_wikipedia ta
 JOIN s_artist a ON ta.id=a.id
-LEFT JOIN bot_wp_artist b ON a.gid = b.gid
+LEFT JOIN bot_wp_artist_link b ON a.gid = b.gid AND b.lang = %s
 WHERE b.gid IS NULL
 ORDER BY a.id
 LIMIT 10000
@@ -76,7 +91,10 @@ JOIN artist_credit_name acn ON r.artist_credit = acn.artist_credit
 WHERE acn.artist = %s
 """
 
-for a_id, a_gid, a_name in db.execute(query):
+params = []
+params.extend(acceptable_countries_for_lang[wp_lang])
+params.extend((wp_lang, wp_lang))
+for a_id, a_gid, a_name in db.execute(query, params):
     out('Looking up artist "%s" http://musicbrainz.org/artist/%s' % (a_name, a_gid))
     matches = wps.query(a_name, defType='dismax', qf='name', rows=50).results
     last_wp_request = time.time()
@@ -90,7 +108,7 @@ for a_id, a_gid, a_name in db.execute(query):
         if delay < 1.0:
             time.sleep(1.0 - delay)
         last_wp_request = time.time()
-        resp = wp.call({'action': 'query', 'prop': 'revisions', 'titles': title, 'rvprop': 'content'})
+        resp = wp.call({'action': 'query', 'prop': 'revisions', 'titles': title.encode('utf8'), 'rvprop': 'content'})
         pages = resp['query']['pages'].values()
         if not pages or 'revisions' not in pages[0]:
             continue
@@ -98,7 +116,7 @@ for a_id, a_gid, a_name in db.execute(query):
         if 'redirect' in page:
             out(' * redirect page, skipping')
             continue
-        if 'disambiguationpages' in page or 'infoboxalbum' in page:
+        if 'disambiguationpages' in page or 'infoboxalbum' in page or 'homonymie' in page:
             out(' * disambiguation or album page, skipping')
             continue
         out(' * trying article "%s"' % (title,))
@@ -121,12 +139,12 @@ for a_id, a_gid, a_name in db.execute(query):
         min_ratio = 0.15 if len(a_name) > 15 else 0.3
         if ratio < min_ratio:
             continue
-        url = 'http://en.wikipedia.org/wiki/%s' % (urllib.quote(page_title.encode('utf8').replace(' ', '_')),)
+        url = 'http://'+wp_lang+'.wikipedia.org/wiki/%s' % (urllib.quote(page_title.encode('utf8').replace(' ', '_')),)
         text = 'Matched based on the name. The page mentions %s.' % (join_names('album', found_albums),)
         out(' * linking to %s' % (url,))
         out(' * edit note: %s' % (text,))
         time.sleep(60 * 3)
         mb.add_url("artist", a_gid, 179, url, text)
         break
-    db.execute("INSERT INTO bot_wp_artist (gid) VALUES (%s)", (a_gid,))
+    db.execute("INSERT INTO bot_wp_artist_link (gid, lang) VALUES (%s, %s)", (a_gid,wp_lang))
 
