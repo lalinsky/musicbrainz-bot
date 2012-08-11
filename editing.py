lalinsky/musicbrainz-bot

commit ff2bc3fbbbdfd63dffe056cfcbc767d520659197

editing: edits_left() now returns tuple (normal_edits_left, edits_left)
To reflect new http://musicbrainz.org/doc/Bots/Code_of_Conduct.

editing.py
@@ -70,11 +70,12 @@ class MusicBrainzClient(object):
         if resp.geturl() != self.url("/user/" + username):
             raise Exception('unable to login')
 
-    # return number of edits that left for today
-    def edits_left(self, max_edits=1000):
+    # return tuple (normal_edits_left, edits_left)
+    def edits_left(self, max_open_edits=2000, max_edits_per_day=1000):
         if self.editor_id is None:
             print 'error, pass editor_id to constructor for edits_left()'
             return 0
+        re_found_edits = re.compile(r'Found (?:at least )?([0-9]+(?:,[0-9]+)?) edits')
         today = datetime.utcnow().strftime('%Y-%m-%d')
         kwargs = {
                 'page': '2000',
@@ -92,11 +93,24 @@ class MusicBrainzClient(object):
         url = self.url("/search/edits", **kwargs)
         self.b.open(url)
         page = self.b.response().read()
-        m = re.search(r'Found (?:at least )?([0-9]+(?:,[0-9]+)?) edits', page)
+        m = re_found_edits.search(page)
         if not m:
-            print 'error, could not determine remaining edits'
+            print 'error, could not determine remaining daily edits'
             return 0
-        return max_edits - int(re.sub(r'[^0-9]+', '', m.group(1)))
+        edits_today = int(re.sub(r'[^0-9]+', '', m.group(1)))
+        edits_left = max_edits_per_day - edits_today
+        if edits_left <= 0:
+            return edits_left
+        url = self.url("/user/%s/edits/open" % (self.username,), page='2000')
+        self.b.open(url)
+        page = self.b.response().read()
+        m = re_found_edits.search(page)
+        if not m:
+            print 'error, could not determine open edits'
+            return 0
+        open_edits = int(re.sub(r'[^0-9]+', '', m.group(1)))
+        normal_edits_left = min(edits_left, max_open_edits - open_edits)
+        return normal_edits_left, edits_left
 
     def add_release(self, album, edit_note, auto=False):
         form = album_to_form(album)

commit 5913fffb2a5ff06ec4e15694d6585fd26c4e4e77

Fix bug in edits_left() (introduced in ff2bc3f).

editing.py
@@ -74,7 +74,7 @@ class MusicBrainzClient(object):
     def edits_left(self, max_open_edits=2000, max_edits_per_day=1000):
         if self.editor_id is None:
             print 'error, pass editor_id to constructor for edits_left()'
-            return 0
+            return 0, 0
         re_found_edits = re.compile(r'Found (?:at least )?([0-9]+(?:,[0-9]+)?) edits')
         today = datetime.utcnow().strftime('%Y-%m-%d')
         kwargs = {
@@ -96,18 +96,18 @@ class MusicBrainzClient(object):
         m = re_found_edits.search(page)
         if not m:
             print 'error, could not determine remaining daily edits'
-            return 0
+            return 0, 0
         edits_today = int(re.sub(r'[^0-9]+', '', m.group(1)))
         edits_left = max_edits_per_day - edits_today
         if edits_left <= 0:
-            return edits_left
+            return 0, 0
         url = self.url("/user/%s/edits/open" % (self.username,), page='2000')
         self.b.open(url)
         page = self.b.response().read()
         m = re_found_edits.search(page)
         if not m:
             print 'error, could not determine open edits'
-            return 0
+            return 0, 0
         open_edits = int(re.sub(r'[^0-9]+', '', m.group(1)))
         normal_edits_left = min(edits_left, max_open_edits - open_edits)
         return normal_edits_left, edits_left