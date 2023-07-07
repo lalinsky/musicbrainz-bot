6bd906b3573b440680adf445bd5bd3eda039379e
Liz Fischer <liz@weareavp.com>
2023-07-07 13:19:13 -0500

    Handle exceptions, save data periodically, add doc strings

--- editing.py
+++ editing.py
@@ -7,21 +7,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import TimeoutException
 
 
 class MusicBrainzClient(object):
     def __init__(self, username, password, server="https://test.musicbrainz.org", headless=False):
         self.server = server
         self.username = username
-        # self.b = mechanize.Browser()
-        # self.b.set_handle_robots(False)
-        # self.b.set_debug_redirects(False)
-        # self.b.set_debug_http(False)
-        # self.b.addheaders = [('User-agent', 'dahr-musicbrainz-bot/1.0 ( %s/user/%s )' % (server, username))]
-
 
         ff_options = Options()
-        ff_options.headless = headless
+        if headless:
+            ff_options.headless = headless
         self.b = webdriver.Firefox(options=ff_options)
         self.login(username, password)
 
@@ -50,16 +46,17 @@ class MusicBrainzClient(object):
         if self.b.current_url != self.url("/user/" + username):
             raise Exception("Unable to login. Is your password correct?")
 
+        logging.info(f"Logged in to MusicBrainz as {self.username} at {self.server}")
+
     # return tuple (normal_edits_left, edits_left)
+    # TODO: This could be more efficient if it used the table on the user page. Fewer page loads.
     def edits_left(self, max_open_edits=2000, max_edits_per_day=1000):
 
         # Check num of edits made today
         re_found_edits = re.compile(r"Found (?:at least )?([0-9]+(?:,[0-9]+)?) edits?")
-        # today = datetime.utcnow().strftime('%Y-%m-%d')
         kwargs = {
             "page": "2000",
             "combinator": "and",
-            # 'negation': '0',
             "conditions.0.field": "open_time",
             "conditions.0.operator": ">",
             "conditions.0.args.0": "today",
@@ -77,6 +74,7 @@ class MusicBrainzClient(object):
         edits_today = int(re.sub(r"[^0-9]+", "", match.group(1)))
         edits_left = max_edits_per_day - edits_today
         if edits_left <= 0:
+            logging.info("No more edits available for today. Try again tomorrow.")
             return 0, 0
 
         # Check number of open edits
@@ -89,6 +87,7 @@ class MusicBrainzClient(object):
             return 0, 0
         open_edits = int(re.sub(r"[^0-9]+", "", match.group(1)))
         normal_edits_left = min(edits_left, max_open_edits - open_edits)
+        logging.info(f"Edits available for today: {edits_left}")
         return normal_edits_left, edits_left
 
     def add_external_link(self, artist_id, link, edit_note=None):
@@ -96,6 +95,9 @@ class MusicBrainzClient(object):
         artist_url = self.url(f"/artist/{artist_id}")
         artist_edit_url = f"{artist_url}/edit"
         self.b.get(artist_edit_url)
+        if self.b.current_url != artist_edit_url and "/artist/" in self.b.current_url:
+            # Artist ID redirected
+            artist_url = self.b.current_url.replace("/edit", "")
 
         # wait for JS to load external links table
         WebDriverWait(self.b, 15).until(
@@ -107,7 +109,7 @@ class MusicBrainzClient(object):
         re_found_dahr_link = re.compile(r"adp.library.ucsb.edu/names")
         dahr_link_found = re_found_dahr_link.search(page)
         if dahr_link_found:
-            logging.info(f"DAHR link already present for MB id {artist_id}")
+            logging.info(f"\tDAHR link already present for MB id {artist_id}")
             return False
 
         # Add URL
@@ -126,23 +128,20 @@ class MusicBrainzClient(object):
         submit_button = self.b.find_element(By.CSS_SELECTOR, "button.submit")
         submit_button.click()
 
-        # wait for edit to go through
-        WebDriverWait(self.b, 20).until(EC.url_changes(artist_edit_url))
+        try:
+            # wait for edit to go through
+            WebDriverWait(self.b, 60).until(EC.url_changes(artist_edit_url))
+        except TimeoutException:
+            logging.error(f"\tEdit timed out for MB entry {artist_id}")
+            raise TimeoutError
+
+        # If we don't end up back on the artist page, something went weird with the edit
         if self.b.current_url != artist_url:
-            raise Exception("Edit failed")
+            raise RuntimeError(f"\tEdit failed for MB entry {artist_id}")
 
-        return True  # success
-
-    # TODO: check/update this function
-    # def cancel_edit(self, edit_nr, edit_note=""):
-    #     self.b.open(self.url("/edit/%s/cancel" % (edit_nr,)))
-    #     page = self.b.response().read()
-    #     self.b.select_form(
-    #         predicate=lambda f: f.method == "POST" and "/cancel" in f.action
-    #     )
-    #     if edit_note:
-    #         self.b["confirm.edit_note"] = edit_note.encode("utf8")
-    #     self.b.submit()
+        # If we get here, the edit was made successfully
+        logging.info(f"\tAdded link to MB entry {artist_id}")
+        return True
 
     def __del__(self):
         # Close selenium when object is removed
--- run_bot.py
+++ run_bot.py
@@ -1,17 +1,26 @@
+"""
+Run a bot to add DAHR identifiers to MusicBrainz.
+"""
+
 import logging
 import configparser
+import json
+import os
 import pandas
-from editing import MusicBrainzClient
 from selenium.webdriver.remote.remote_connection import LOGGER as SELENIUM_LOGGER
+from editing import MusicBrainzClient
 
 SELENIUM_LOGGER.setLevel(logging.CRITICAL)
 
 
-def load_starting_data(config: configparser.ConfigParser):
+def load_starting_data(config: configparser.ConfigParser) -> list:
     """
-
+    Load DAHR data.
     Args:
-        config (configparser.ConfigParser):
+        config (configparser.ConfigParser): Loaded configuration info
+
+    Returns:
+        List of dicts mapping DAHR IDs (key "dahr") to MusicBrainz IDs (key "mb")
     """
 
     # Load DAHR data
@@ -32,23 +41,85 @@ def load_starting_data(config: configparser.ConfigParser):
     return id_mappings.to_dict("records")
 
 
-def init_mb_client(config: configparser.ConfigParser):
+def init_mb_client(config: configparser.ConfigParser) -> MusicBrainzClient:
+    """
+    Initalize a MusicBrainzClient, which includes starting Selenium and logging in to MusicBrainz.
+
+    Args:
+        config: Loaded configuration info
+
+    Returns:
+        MusicBrainzClient, signed in with the information specified in config.
+    """
     mb_user = config.get("musicbrainz", "username")
     mb_pw = config.get("musicbrainz", "password")
     mb_server = config.get("musicbrainz", "server")
-    headless = config.get("general", "headless")
+    headless = config.get("general", "headless") == "True"
     return MusicBrainzClient(mb_user, mb_pw, server=mb_server, headless=headless)
 
 
-def main():
+def save_progress(config: configparser.ConfigParser, checked: list, modified: list):
+    """
+    Saves list of checked entries & modified entries to files specified in config.
+    Args:
+        config: Loaded configuration info
+        checked: List of id mappings already checked
+        modified: List of id mappings that we modified
+    """
+    checked_out = config.get("general", "checked_file")
+    with open(checked_out, "w", encoding="utf-8") as checked_file:
+        json.dump(checked, checked_file, indent=4)
+
+    modified_out = config.get("general", "modified_file")
+    with open(modified_out, "w", encoding="utf-8") as mod_file:
+        json.dump(modified, mod_file, indent=4)
+
+    logging.info(f"Progress saved to {checked_out} and {modified_out}")
+
+
+def load_progress(config: configparser.ConfigParser) -> (list, list):
+    """
+    Load previously checked and modified entries from files specified in config.
+    Args:
+        config:  Loaded configuration info
+
+    Returns:
+        List of previously checked id mappings, and list of previously modified id mappings
+    """
+    checked_out = config.get("general", "checked_file")
+    if os.path.isfile(checked_out):
+        with open(checked_out, "r", encoding="utf-8") as checked_file:
+            checked = json.load(checked_file)
+        logging.info(f"Checked entries loaded from {checked_out}")
+
+    else:
+        checked = []
+
+    modified_out = config.get("general", "modified_file")
+    if os.path.isfile(modified_out):
+        with open(modified_out, "r", encoding="utf-8") as mod_file:
+            modified = json.load(mod_file)
+        logging.info(f"Modified entries loaded from {modified_out}")
+
+    else:
+        modified = []
+
+    return checked, modified
+
+
+def run():
+    """
+    Main method for bot.
+    """
+
     # Init logging
-    logging.basicConfig(encoding='utf-8', level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
-                        handlers=[
-                            logging.FileHandler("bot.log"),
-                            logging.StreamHandler()
-                        ]
-                    )
+    logging.basicConfig(
+        encoding="utf-8",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+        handlers=[logging.FileHandler("bot.log"), logging.StreamHandler()],
+    )
 
     # Load config
     config = configparser.ConfigParser()
@@ -57,20 +128,62 @@ def main():
     # Load DAHR data
     id_mappings = load_starting_data(config)
 
+    # Check for existing saved data
+    checked, modified = load_progress(config)
+    errors = []
+
+    # Get mappings that aren't in the saved data
+    unchecked_mappings = [mapping for mapping in id_mappings if mapping not in checked]
+    if not unchecked_mappings:
+        logging.info(
+            "All entries already checked. "
+            "To rerun, clear the saved data files specified in 'config.ini'"
+        )
+        return
+    # Log number of already-checked entries
+    if checked:
+        logging.info(f"Already checked {len(checked)}/{len(id_mappings)} entries")
+
     # Init MB client
     mb_client = init_mb_client(config)
-    logging.info(f"Logged in to MusicBrainz as {mb_client.username} at {mb_client.server}")
     edits_left = mb_client.edits_left()[0]
     if edits_left <= 0:
-        logging.info("No more edits available for today.")
         return
-    logging.info(f"Edits available for today: {edits_left}")
 
     # Editing loop
-    for entry in id_mappings:
+    for num, entry in enumerate(unchecked_mappings):
+        # If we're out of edits for the day, stop
+        if edits_left <= 0:
+            logging.warning("Out of edits for today. Try again later.")
+            save_progress(config, checked, modified)
+            break
+
+        # Check an entry
         logging.info(f"Checking MB entry {entry['mb']}")
-        mb_client.add_external_link(entry["mb"], entry["dahr"])
+        try:
+            link_added = mb_client.add_external_link(entry["mb"], entry["dahr"])
+            checked.append(entry)
+            if link_added:
+                modified.append(entry)
+                edits_left -= 1
+
+        # If the edit request times out or otherwise failed, the entry wasn't fully checked.
+        # Save it for the next run.
+        except (TimeoutError, RuntimeError):
+            errors.append(entry)
+
+        # Save progress every 20 people
+        if (num + 1) % 3 == 0:
+            save_progress(config, checked, modified)
+
+    if errors:
+        logging.error(f"{len(errors)} were not checked successfully. Please run again.")
+    else:
+        logging.info(
+            f"{len(checked)}/{len(id_mappings)} checked successfully. "
+            f"{len(modified)} links added."
+        )
 
 
 if __name__ == "__main__":
-    main()
+    run()
